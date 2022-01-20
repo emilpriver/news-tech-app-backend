fde7ba14110b7367ec6dd1d3c0b87828ebdadffe
Emil Privér <emil@rivercode.se>
2022-01-20 15:55:24 +0100

    Added News model

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,19 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export async function handleEvent(event: FetchEvent): Promise<Response> {
+  const { request } = event
+
+  event.waitUntil(
+    prisma.log.create({
+      data: {
+        level: 'Info',
+        message: `${request.method} ${request.url}`,
+        meta: {
+          headers: JSON.stringify(request.headers),
+        },
+      },
+    })
+  )
+  return new Response(`request method: ${request.method}!`)
 }
